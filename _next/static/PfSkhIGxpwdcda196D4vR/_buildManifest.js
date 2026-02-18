2305312f594e92657d9a41b022d1f12f98406eca
Jameslead00 <james.lead@gmail.com>
2026-02-18 22:21:07 +0100

    Updates

---
+++ _next/static/PfSkhIGxpwdcda196D4vR/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/schlecht-movers//_next/:path+",
+        "destination": "/schlecht-movers/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
